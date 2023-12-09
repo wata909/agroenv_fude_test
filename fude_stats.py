112cb08681ab849cfc44a0022393f2d091a4aebd
Nobusuke IWASAKI <wata909@gmail.com>
2023-12-09 17:02:36 +0900

    update stats and yml
    update for original resolution
    update yml for update result

--- fude_stats.py
+++ fude_stats.py
@@ -53,10 +53,10 @@ def generate_fude_png(src: str, dst: str, geometry_wkt: str, resampling="nearest
 
     return dst
 
-def increase_resolution(src: str, dst: str, scale_factor=10, resampling="bilinear") -> str:
-    """
-    ラスターデータの解像度を高解像度に変更する
-    """
+# github actionでやるので，ここは使わない
+"""
+def increase_resolution(src: str, dst: str, scale_factor=10, resampling="nearest") -> str:
+    # ラスターデータの解像度を高解像度に変更する
     # 元のラスターの解像度を取得
     src_ds = gdal.Open(src)
     transform = src_ds.GetGeoTransform()
@@ -70,13 +70,14 @@ def increase_resolution(src: str, dst: str, scale_factor=10, resampling="bilinea
     gdal.Warp(dst, src, options=warp_options)
 
     return dst
-
+"""
 def process_feature(mesh_dir, feature, feature_id, high_res_dem, high_res_slope, high_res_direction, high_res_geology):
     """
     各フィーチャーに対する処理
     """
     # 出力フォルダのパスを生成
-    output_feature_dir = os.path.join("meshdata", mesh_dir, "fude_data", str(feature_id))
+#    output_feature_dir = os.path.join("meshdata", mesh_dir, "fude_data", str(feature_id))
+    output_feature_dir = os.path.join(OUTPUT_DIR, mesh_dir, str(feature_id))
     os.makedirs(output_feature_dir, exist_ok=True)
 
     # 高解像度のラスターデータを切り抜き
@@ -96,6 +97,36 @@ def main():
     for mesh_dir in os.listdir(INPUT_MESH_DIR):
         mesh_path = os.path.join(INPUT_MESH_DIR, mesh_dir)
         if os.path.isdir(mesh_path):
+            # ラスターデータのパスを定義
+            dem = os.path.join(INPUT_MESH_DIR, mesh_dir, "dem.png")
+            slope = os.path.join(INPUT_MESH_DIR, mesh_dir, "slope.png")
+            direction = os.path.join(INPUT_MESH_DIR, mesh_dir, "direction.png")
+            geology = os.path.join(INPUT_MESH_DIR, mesh_dir, "geology.png")
+
+            # jsonファイルの読み込み
+            geojson_file = os.path.join(GEOJSON_DIR, f"{mesh_dir}.geojson")
+            if os.path.exists(geojson_file):
+                # GeoJSONファイルの読み込み
+                gdf = gpd.read_file(geojson_file)
+                # 各地物に対する処理
+                for index, feature in gdf.iterrows():
+                    process_feature(mesh_dir, feature, feature['polygon_uuid'], dem, slope, direction, geology)
+            else:
+                # ログに記録
+                with open(LOG_FILE, 'a') as log_file:
+                    log_file.write(f"GeoJSON file not found for mesh {mesh_dir}\n")
+
+"""
+高解像度用の処理。ここではコメントアウト
+def main():
+    # ログファイルの初期化
+    with open(LOG_FILE, 'w') as log_file:
+        log_file.write("Processing Log\n")
+
+    # 各メッシュフォルダに対する処理
+    for mesh_dir in os.listdir(INPUT_MESH_DIR):
+        mesh_path = os.path.join(INPUT_MESH_DIR, mesh_dir)
+        if os.path.isdir(mesh_path):
             # 各ラスターデータの解像度を高解像度に変更
             high_res_dem = increase_resolution(os.path.join(INPUT_MESH_DIR, mesh_dir, "dem.png"), 
                                                os.path.join(mesh_path, "high_res_dem.png"))
@@ -106,17 +137,20 @@ def main():
             high_res_geology = increase_resolution(os.path.join(INPUT_MESH_DIR, mesh_dir, "geology.png"), 
                                                    os.path.join(mesh_path, "high_res_geology.png"))
 
+            # jsonファイルの読み込み
             geojson_file = os.path.join(GEOJSON_DIR, f"{mesh_dir}.geojson")
             if os.path.exists(geojson_file):
                 # GeoJSONファイルの読み込み
                 gdf = gpd.read_file(geojson_file)
                 # 各地物に対する処理
                 for index, feature in gdf.iterrows():
-                    process_feature(mesh_dir, feature, feature['polygon_uuid'], high_res_dem, high_res_slope, high_res_direction, high_res_geology)
+#                    process_feature(mesh_dir, feature, feature['polygon_uuid'], high_res_dem, high_res_slope, high_res_direction, high_res_geology)
+                    process_feature(mesh_dir, feature, feature['polygon_uuid'], dem, slope, direction, geology)
             else:
                 # ログに記録
                 with open(LOG_FILE, 'a') as log_file:
                     log_file.write(f"GeoJSON file not found for mesh {mesh_dir}\n")
+"""
 
 if __name__ == "__main__":
     main()
